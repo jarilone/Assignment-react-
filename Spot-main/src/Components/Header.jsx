c0085935c10a08dc873632d44d6d49a64dd02c4a
Michael Jari <ritdajari@gmail.com>
2025-06-06 18:08:04 +0100

    Add files via upload

---
+++ Spot-main/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../../src/assets/images/icons/Group 13.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={Logo} alt="" />
+      <h3>SPOTS</h3>
+    </header>
+  );
+};
+
+export default Header;
